9b1b072653b076e7576cb9cdb8f2ce2949c026ee
Berlindraft <xraymundzyron@gmail.com>
2025-03-11 01:32:56 +0800

    database connection

--- cpenrolmentv1/settings.py
+++ cpenrolmentv1/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'cpenrolmentv1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cpenrolment',
+        'USER': 'postgres',
+        'PASSWORD': 'zyron314159',
+        'HOST': 'localhost',
+        'PORT': '',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
 
